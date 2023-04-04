14b74a695d1c654f184e3560d1b858d95d1d95ff
Peter Park <peterjpark@pm.me>
2023-04-04 13:03:19 -0400

    Add comments to script

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,13 @@
-dayjs.extend(window.dayjs_plugin_advancedFormat)
-dayjs.extend(window.dayjs_plugin_customParseFormat)
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+dayjs.extend(window.dayjs_plugin_customParseFormat);
 
 $(function () {
   var savedSched = {};
   var blockStyle;
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  // Apply the past, present, or future blockStyle to each timeblock by comparing the id to the current hour.
   for (var blockHour = 9; blockHour <= 17; blockHour++) {
+    // Compare current hour to timeblock hours.
     if (blockHour < dayjs().hour()) {
       blockStyle = "past";
     } else if (blockHour > dayjs().hour()) {
@@ -20,9 +16,10 @@ $(function () {
       blockStyle = "present";
     }
 
+    // Fill in the id, class, and hour text. Add timeblock for 9AM - 5PM.
     $("#container").append(
       `<div id="hour-${blockHour}" class="row time-block ${blockStyle}">
-        <div class="col-2 col-md-1 hour text-center py-3">${dayjs(blockHour.toString(), "H").format("hA")}</div>
+        <div class="col-2 col-md-1 hour text-center py-3">${dayjs(blockHour.toString(),"H").format("hA")}</div>
         <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
@@ -34,7 +31,7 @@ $(function () {
   // Display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 
-  // Get user input from local storage and populate timeblocks.
+  // Get saved input from local storage and populate timeblocks.
   if (localStorage.getItem("savedSched")) {
     savedSched = JSON.parse(localStorage.getItem("savedSched"));
   }
@@ -43,7 +40,7 @@ $(function () {
     $(`#${key}`).children(".description").val(savedSched[key]);
   }
 
-  // On click, save user input in local storage. {timeBlock: description}.
+  // On click, save user input in local storage. savedSched: {timeBlock: description}.
   $(".saveBtn").on("click", function () {
     var timeBlock = $(this).parent().attr("id");
     var description = $(this).siblings(".description").val().trim();
