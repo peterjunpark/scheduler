065f35532fb57f323ad0fd20feb25569c9f03e4c
Peter Park <peterjpark@pm.me>
2023-04-04 10:12:10 -0400

    Add js to populate and style timeblocks

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+dayjs.extend(window.dayjs_plugin_advancedFormat)
+dayjs.extend(window.dayjs_plugin_customParseFormat)
+
 $(function () {
   var savedSched = {};
-  // On click, save user input in local storage. {timeBlock: description}.
-  $(".saveBtn").on("click", function () {
-    var timeBlock = $(this).parent().attr("id");
-    var description = $(this).siblings(".description").val().trim();
-    savedSched[timeBlock] = description;
-    localStorage.setItem("savedSched", JSON.stringify(savedSched))
-  });
-  //
+  var blockStyle;
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  //
+  for (var blockHour = 9; blockHour <= 17; blockHour++) {
+    if (blockHour < dayjs().hour()) {
+      blockStyle = "past";
+    } else if (blockHour > dayjs().hour()) {
+      blockStyle = "future";
+    } else {
+      blockStyle = "present";
+    }
+
+    $("#container").append(
+      `<div id="hour-${blockHour}" class="row time-block ${blockStyle}">
+        <div class="col-2 col-md-1 hour text-center py-3">${dayjs(blockHour.toString(), "H").format("hA")}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>`
+    );
+  }
+
+  // Display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 
   // Get user input from local storage and populate timeblocks.
   if (localStorage.getItem("savedSched")) {
@@ -28,6 +43,11 @@ $(function () {
     $(`#${key}`).children(".description").val(savedSched[key]);
   }
 
-  // Display the current date in the header of the page.
-  $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
+  // On click, save user input in local storage. {timeBlock: description}.
+  $(".saveBtn").on("click", function () {
+    var timeBlock = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val().trim();
+    savedSched[timeBlock] = description;
+    localStorage.setItem("savedSched", JSON.stringify(savedSched));
+  });
 });
