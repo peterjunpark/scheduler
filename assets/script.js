84d6eed67dd56c2cb79ce216429f7e387243c5d4
Peter Park <peterjpark@pm.me>
2023-04-04 01:14:25 -0400

    Add logic to handle no local storage data

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@
 // in the html.
 $(function () {
   var savedSched = {};
-
   // On click, save user input in local storage. {timeBlock: description}.
   $(".saveBtn").on("click", function () {
     var timeBlock = $(this).parent().attr("id");
@@ -17,14 +16,15 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  savedSched = JSON.parse(localStorage.getItem("savedSched"));
-  console.log(savedSched);
+
+  // Get user input from local storage and populate timeblocks.
+  if (localStorage.getItem("savedSched")) {
+    savedSched = JSON.parse(localStorage.getItem("savedSched"));
+  }
+
   for (key in savedSched) {
-    console.log($(`#${key}`));
     $(`#${key}`).children(".description").val(savedSched[key]);
   }
 
