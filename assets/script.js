e39d2e05835cb6537a7bc96ffc9303be55e4ed0d
Peter Park <peterjpark@pm.me>
2023-04-04 00:36:23 -0400

    Add logic to save timeblock to localStorage

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,19 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var savedSched = {};
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $(".saveBtn").on("click", function () {
+    var timeBlock = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val().trim();
+    savedSched[timeBlock] = description;
+    localStorage.setItem("savedSched", JSON.stringify(savedSched))
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -19,6 +26,6 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // DONE: Add code to display the current date in the header of the page.
+  // Add code to display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 });
