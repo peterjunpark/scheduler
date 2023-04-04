0ee0d07f2c4511a3545bf38710acae25b6b13fec
Peter Park <peterjpark@pm.me>
2023-04-04 01:01:54 -0400

    Add logic to populate descriptions from local stor

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,8 @@
 // in the html.
 $(function () {
   var savedSched = {};
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
+  // On click, save user input in local storage. {timeBlock: description}.
   $(".saveBtn").on("click", function () {
     var timeBlock = $(this).parent().attr("id");
     var description = $(this).siblings(".description").val().trim();
@@ -25,7 +21,13 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // Add code to display the current date in the header of the page.
+  savedSched = JSON.parse(localStorage.getItem("savedSched"));
+  console.log(savedSched);
+  for (key in savedSched) {
+    console.log($(`#${key}`));
+    $(`#${key}`).children(".description").val(savedSched[key]);
+  }
+
+  // Display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 });
